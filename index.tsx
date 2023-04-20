125e546c002713568d78a71a394df95480d78e2d
NoirKye <130001772+NoirKye@users.noreply.github.com>
2023-04-20 15:20:54 +1200

    3 files modified

--- index.tsx
+++ index.tsx
@@ -3,14 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import Headers1 from './header';
+import Header1 from './header';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Headers1 />
+    <Header1 />
     <App />
   </StrictMode>
 );
